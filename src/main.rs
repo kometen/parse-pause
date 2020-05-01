3a7b3339d11a3f5776247aa145efe10ee3d33a2b
Claus Guttesen <kometen@gmail.com>
2020-05-01 17:07:26 +0200

    Add segment to a struct. Push struct to a vector.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ use xml::reader::{EventReader, XmlEvent};
 fn main() {
     println!("Jeg æder blåbærsyltetøj!");
 
+    struct Segment {
+        offset: String,
+        timeslot: [u32; 2]
+    }
+
+    let mut segment_vector: Vec<Segment> = Vec::new();
+
     let keyword = "silence";
 
     // Command line parameters.
@@ -33,13 +40,23 @@ fn main() {
     let file = BufReader::new(file);
     let parser = EventReader::new(file);
 
+    // Initialise with first offset (chapter).
+    let segment = Segment{ offset: "PT0S".to_string(), timeslot: [0,0] };
+    segment_vector.push(segment);
+
     for e in parser {
         match e {
             Ok(XmlEvent::StartElement { name, attributes, .. }) => {
                 let name = name.local_name;
                 if name.eq(keyword) {
                     print!("{}", name);  // Tag; ie. silence
-                    // name can be from, until. value is iso8601-formatted duration.
+
+                    // name can be [ from | until ]. value is iso8601-formatted duration.
+                    // Use an array to store from at [0] and until at [1].
+
+                    let mut index = 0;
+                    let mut segment = Segment{ offset: "".to_string(), timeslot: [0, 0]};
+
                     for attribute in attributes {
                         print!(":{}={}", attribute.name, attribute.value);
                         let _ = match iso8601::duration(&attribute.value) {
@@ -49,14 +66,21 @@ fn main() {
                             Ok(v) => {
                                 //print!("Date: {:?}", v);
                                 match v {
-                                    iso8601::Duration::YMDHMS{year, month, day, hour, minute, second, millisecond} =>
-                                        print!("hour: {}, minute: {}, second: {}, millisecond: {}", hour, minute, second, millisecond),
+                                    iso8601::Duration::YMDHMS{year, month, day, hour, minute, second, millisecond} => {
+                                        let milliseconds = (hour * 3600 + minute * 60 + second) * 1000 + millisecond;
+                                        print!("hour: {}, minute: {}, second: {}, millisecond: {}, milliseconds: {}", hour, minute, second, millisecond, milliseconds);
+                                        // Only store from in offset.
+                                        if index == 0 { segment.offset = attribute.value; }
+                                        segment.timeslot[index] = milliseconds;
+                                    },
                                     iso8601::Duration::Weeks(w) => print!("weeks: {}", w)
                                 };
+                                index += 1;
                             }
                         };
                     }
                     println!();
+                    segment_vector.push(segment);
                 }
             }
             Err(e) => {
@@ -65,5 +89,9 @@ fn main() {
             }
             _ => {}
         }
+    }
+    for segment in segment_vector {
+        let duration: u32 = segment.timeslot[1] - segment.timeslot[0];
+        println!("offset: {}, duration: {}", segment.offset, duration);
     }
 }
